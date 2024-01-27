9b36d047f95ac9e822715ff09236351510c04103
IvanVazquezL <ivanvl_11@hotmail.com>
2024-01-27 15:53:00 -0600

    Create Database

---
+++ src/CreateDatabase/Main.java
@@ -0,0 +1,56 @@
+package CreateDatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:./src/carsharing/db/carsharing";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL);
+
+            //STEP 3: Execute a query
+            stmt = conn.createStatement();
+
+            System.out.println("Creating table in given database...");
+            String createTable =  "CREATE TABLE COMPANY " +
+                    "(ID INTEGER AUTO_INCREMENT, " +
+                    " NAME VARCHAR(255) UNIQUE not NULL," +
+                    "PRIMARY KEY ( id ))";
+            stmt.executeUpdate(createTable);
+            System.out.println("Created table in given database...");
+
+            // STEP 4: Clean-up environment
+            stmt.close();
+            conn.close();
+        } catch(SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch(Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try{
+                if(stmt!=null) stmt.close();
+            } catch(SQLException se2) {
+            } // nothing we can do
+            try {
+                if(conn!=null) conn.close();
+            } catch(SQLException se){
+                se.printStackTrace();
+            } //end finally try
+        } //end try
+    }
+}
